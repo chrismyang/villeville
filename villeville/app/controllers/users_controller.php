d79f06d6907e292a04c69abca7eb0aa739923275
tingyutlin <tingyutlin@gmail.com>
2010-12-30 18:48:44 -0800

    fixing white space in users_controller.php

--- villeville/app/controllers/users_controller.php
+++ villeville/app/controllers/users_controller.php
@@ -1,15 +1,13 @@
 <?php
 class UsersController extends AppController {
- 
-  function register() {
-
-	if (!empty($this->params['form']) && !empty($this->params['form']['submit'])) {
-	     if ($this->User->save($this->params['form'])) {
-	             $this->flash('Your registration information was accepted.', '/users/register');
-             } else {
-	             $this->flash('There was a problem with your registration', '/users/register');
-	     }
+	function register() {
+		if (!empty($this->params['form']) && !empty($this->params['form']['submit'])) {
+			if ($this->User->save($this->params['form'])) {
+				$this->flash('Your registration information was accepted.', '/users/register');
+			} else {
+				$this->flash('There was a problem with your registration', '/users/register');
+			}
+		}
 	}
-  }
 }
 ?>
